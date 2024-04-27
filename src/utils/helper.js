b611d843db66f23c19b49ee156ae17e99dcdc4f3
Divyanshu MJ <141554205+Divyanshu708@users.noreply.github.com>
2024-04-28 01:48:52 +0530

    Update helper.js

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -22,8 +22,3 @@ export function getWeatherIcon(wmoCode) {
   return icons.get(arr);
 }
 
-export function formatDay(dateStr) {
-  return new Intl.DateTimeFormat("en", {
-    weekday: "short",
-  }).format(new Date(dateStr));
-}
